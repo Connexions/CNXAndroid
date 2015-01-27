a4fccf32382bf4bb55fd026ba17aa3d81dc0534d
edwoodward <ecw1@rice.edu>
2015-01-26 20:32:24 -0600

    removed icon name which was causing an error in Favorites

--- app/src/main/java/org/cnx/android/activity/LandingActivity.java
+++ app/src/main/java/org/cnx/android/activity/LandingActivity.java
@@ -247,35 +247,35 @@ public class LandingActivity extends Activity
             //c.setUrl(new URL("http://archive.alpha.cnx.org:6543/contents/031da8d3-b525-429c-80cf-6c8ed997733a@7.31.html"));
             c.setUrl(new URL("http://m.cnx.org/content/col11406/latest"));
             c.setIconDrawable(R.drawable.physics_lg);
-            c.setIcon("physics");
+            //c.setIcon("physics");
 
             Content c2 = new Content();
             c2.setTitle(getString(R.string.sociology));
             c2.setContentString(getString(R.string.sociology_desc));
             c2.setUrl(new URL("http://m.cnx.org/content/col11407/latest/"));
             c2.setIconDrawable(R.drawable.sociology_lg);
-            c2.setIcon("sociology");
+            //c2.setIcon("sociology");
 
             Content c3 = new Content();
             c3.setTitle(getString(R.string.biology));
             c3.setContentString(getString(R.string.biology_desc));
             c3.setUrl(new URL("http://m.cnx.org/content/col11448/latest/"));
             c3.setIconDrawable(R.drawable.biology_lg);
-            c3.setIcon("biology");
+            //c3.setIcon("biology");
 
             Content c4 = new Content();
             c4.setTitle(getString(R.string.concepts_biology));
             c4.setContentString(getString(R.string.concepts_biology_desc));
             c4.setUrl(new URL("http://m.cnx.org/content/col11487/latest/"));
             c4.setIconDrawable(R.drawable.concepts_biology_lg);
-            c4.setIcon("concepts");
+            //c4.setIcon("concepts");
 
             Content c5 = new Content();
             c5.setTitle(getString(R.string.anatomy));
             c5.setContentString(getString(R.string.anatomy_desc));
             c5.setUrl(new URL("http://m.cnx.org/content/col11496/latest/"));
             c5.setIconDrawable(R.drawable.anatomy_lg);
-            c5.setIcon("anatomy");
+            //c5.setIcon("anatomy");
 
             Content c6 = new Content();
             c6.setTitle(getString(R.string.statistics));
@@ -283,28 +283,28 @@ public class LandingActivity extends Activity
             //c6.setUrl(new URL("http://cnx.org/contents/30189442-6998-4686-ac05-ed152b91b9de@16.5"));
             c6.setUrl(new URL("http://m.cnx.org/content/col11562/latest/"));
             c6.setIconDrawable(R.drawable.statistics_lg);
-            c6.setIcon("statistics");
+            //c6.setIcon("statistics");
 
             Content c7 = new Content();
             c7.setTitle(getString(R.string.econ));
             c7.setContentString(getString(R.string.economics_desc));
             c7.setUrl(new URL("http://m.cnx.org/content/col11613/latest/"));
             c7.setIconDrawable(R.drawable.econ_lg);
-            c7.setIcon("econ");
+            //c7.setIcon("economics");
 
             Content c11 = new Content();
             c11.setTitle(getString(R.string.macro_econ));
             c11.setContentString(getString(R.string.macro_desc));
             c11.setUrl(new URL("http://m.cnx.org/content/col11626/latest/"));
             c11.setIconDrawable(R.drawable.macro_econ_lg);
-            c11.setIcon("macro");
+            //c11.setIcon("macro");
 
             Content c12 = new Content();
             c12.setTitle(getString(R.string.micro_econ));
             c12.setContentString(getString(R.string.micro_desc));
             c12.setUrl(new URL("http://m.cnx.org/content/col11627/latest/"));
             c12.setIconDrawable(R.drawable.micro_econ_lg);
-            c12.setIcon("micro");
+            //c12.setIcon("micro");
 
             Content c8 = new Content();
             c8.setTitle(getString(R.string.precalculus));
@@ -323,12 +323,14 @@ public class LandingActivity extends Activity
             c10.setContentString(getString(R.string.history_desc));
             c10.setUrl(new URL("http://m.cnx.org/content/col11740/latest/"));
             c10.setIconDrawable(R.drawable.history_lg);
+            //c10.setIcon("history");
 
             Content c13 = new Content();
             c13.setTitle(getString(R.string.psychology));
             c13.setContentString(getString(R.string.psychology_desc));
             c13.setUrl(new URL("http://m.cnx.org/content/col11629/latest/"));
             c13.setIconDrawable(R.drawable.psychology_lg);
+            //c13.setIcon("psychology");
 
             Content c14 = new Content();
             c14.setTitle(getString(R.string.bus_fundamentals));
