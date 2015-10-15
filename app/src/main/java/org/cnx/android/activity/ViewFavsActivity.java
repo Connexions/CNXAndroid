2af128fb9fc039421474a8d0e9c577d168704733
edwoodward <ecw1@rice.edu>
2015-10-14 21:10:18 -0500

    Changes for favorites fragment

--- app/src/main/java/org/cnx/android/activity/ViewFavsActivity.java
+++ app/src/main/java/org/cnx/android/activity/ViewFavsActivity.java
@@ -7,53 +7,32 @@
 package org.cnx.android.activity;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 
 import android.app.ActionBar;
 import android.app.Activity;
+import android.app.FragmentTransaction;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v4.widget.DrawerLayout;
-import android.support.v7.widget.DefaultItemAnimator;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
-import android.support.v7.widget.helper.ItemTouchHelper;
 import android.view.*;
 import android.widget.SimpleAdapter;
 import org.cnx.android.R;
-import org.cnx.android.adapters.FavsRecyclerViewAdapter;
-import org.cnx.android.beans.Content;
+import org.cnx.android.fragments.FavsFragment;
 import org.cnx.android.handlers.MenuHandler;
-import org.cnx.android.providers.Favs;
-import org.cnx.android.providers.utils.DBUtils;
 
 import android.content.Intent;
-import android.database.Cursor;
 import android.os.Bundle;
-import android.os.Handler;
-import android.util.Log;
 import android.widget.AdapterView;
 import android.widget.ListView;
 
-import co.paulburke.android.itemtouchhelperdemo.helper.OnStartDragListener;
-import co.paulburke.android.itemtouchhelperdemo.helper.SimpleItemTouchHelperCallback;
-
 /**
+ * Activity for displaying Favs Fragment
  * @author Ed Woodward
  *
  */
-public class ViewFavsActivity extends Activity implements OnStartDragListener
+public class ViewFavsActivity extends Activity
 {
-    FavsRecyclerViewAdapter adapter;
-    RecyclerView recyclerView;
-    /** list of lenses as Content objects */ 
-    ArrayList<Content> content;
-    
-    /**handler */
-    final private Handler handler = new Handler();
-
-    private ItemTouchHelper itemTouchHelper;
 
     private List<HashMap<String,String>> navTitles;
     private DrawerLayout drawerLayout;
@@ -61,15 +40,6 @@ public class ViewFavsActivity extends Activity implements OnStartDragListener
     String[] from = {"nav_icon","nav_item"};
     int[] to = { R.id.nav_icon , R.id.nav_item};
     
-    /** Inner class for completing load work */
-    private Runnable finishedLoadingListTask = new Runnable() 
-    {
-        public void run() 
-        {
-          finishedLoadingList();
-        }
-      };
-      
       /* (non-Javadoc)
        * @see android.app.Activity#onCreate(android.os.Bundle)
        * Called when the activity is first created.
@@ -79,31 +49,15 @@ public class ViewFavsActivity extends Activity implements OnStartDragListener
       {
           super.onCreate(savedInstanceState);
           requestWindowFeature(Window.FEATURE_INDETERMINATE_PROGRESS);
-          setContentView(R.layout.card_view);
+          setContentView(R.layout.favs_activity);
           
           ActionBar aBar = getActionBar();
           aBar.setTitle("  " + getString(R.string.title_favs));
-          recyclerView = (RecyclerView)findViewById(R.id.recycler_view);
-          recyclerView.setLayoutManager(new LinearLayoutManager(this));
-          recyclerView.setItemAnimator(new DefaultItemAnimator());
-          setProgressBarIndeterminateVisibility(true);
-          //get already retrieved feed and reuse if it is there
-          if(content == null)
-          {
-              //no previous data, so database must be read
-              readDB();
-          }
-          else
-          {
-              //reuse existing feed data
-              adapter = new FavsRecyclerViewAdapter(content, R.layout.card_row, this);
-              recyclerView.setAdapter(adapter);
-              ItemTouchHelper.Callback callback = new SimpleItemTouchHelperCallback(adapter);
-              itemTouchHelper = new ItemTouchHelper(callback);
-              itemTouchHelper.attachToRecyclerView(recyclerView);
-              setProgressBarIndeterminateVisibility(false);
-             
-          }
+
+          FragmentTransaction transaction = getFragmentManager().beginTransaction();
+          FavsFragment fragment = new FavsFragment();
+          transaction.add(R.id.favsFragment, fragment);
+          transaction.commit();
 
           String[] items = getResources().getStringArray(R.array.nav_list);
           setDrawer(items);
@@ -147,16 +101,6 @@ public class ViewFavsActivity extends Activity implements OnStartDragListener
           
       }
 
-    /**
-     * For OnStartDragListener
-     * @param viewHolder The holder of the view to drag.
-     */
-    @Override
-    public void onStartDrag(RecyclerView.ViewHolder viewHolder)
-    {
-        itemTouchHelper.startDrag(viewHolder);
-    }
-      
       /* (non-Javadoc)
        * @see android.app.Activity#onOptionsItemSelected(android.view.MenuItem)
        */
@@ -175,80 +119,6 @@ public class ViewFavsActivity extends Activity implements OnStartDragListener
 
       }
       
-      /* (non-Javadoc)
-     * @see android.app.Activity#onResume()
-     */
-    @Override
-      protected void onResume()
-      {
-          super.onResume();
-          //if database state has changed, reload the display
-          if(content != null)
-          {
-              int dbCount = getDBCount();
-              
-              if(dbCount >  content.size())
-              {
-                  readDB();
-              }
-          }
-      }
-      
-
-      
-      /** Actions after list is loaded in View*/
-      protected void finishedLoadingList() 
-      {
-          recyclerView.setAdapter(adapter);
-          ItemTouchHelper.Callback callback = new SimpleItemTouchHelperCallback(adapter);
-          itemTouchHelper = new ItemTouchHelper(callback);
-          itemTouchHelper.attachToRecyclerView(recyclerView);
-          setProgressBarIndeterminateVisibility(false);
-      }
-      
-      /** reads feed in a separate thread.  Starts progress dialog*/
-      private void readDB()
-      {
-
-          Thread loadFavsThread = new Thread() 
-          {
-            public void run() 
-            {
-                
-                content = DBUtils.readCursorIntoList(getContentResolver().query(Favs.CONTENT_URI, null, null, null, null));
-                
-               Collections.sort(content);
-                
-                fillData(content);
-                handler.post(finishedLoadingListTask);
-            }
-          };
-          loadFavsThread.start();
-          
-      }
-      /**
-       * Loads feed data into adapter on initial reading of feed
-       * @param contentList ArrayList<Content>
-       */
-      private void fillData(ArrayList<Content> contentList) 
-      {
-          //Log.d("LensViewer", "fillData() called");
-          adapter = new FavsRecyclerViewAdapter(contentList, R.layout.card_row,this);
-      }
-      
-      /**
-       * Queries the database to get the number of favorites stored
-     * @return int - the number of favorites items in the database
-     */
-    private int getDBCount()
-    {
-          Cursor c = getContentResolver().query(Favs.CONTENT_URI, null, null, null, null);
-          int count = c.getCount();
-          c.close();
-          return count;
-          
-    }
-
     private void selectItem(int position)
     {
         switch (position)
---
+++ app/src/main/java/org/cnx/android/fragments/FavsFragment.java
@@ -0,0 +1,180 @@
+/**
+ * Copyright (c) 2015 Rice University
+ *
+ * This software is subject to the provisions of the GNU Lesser General
+ * Public License Version 2.1 (LGPL).  See LICENSE.txt for details.
+ */
+package org.cnx.android.fragments;
+
+import android.app.Activity;
+import android.app.Fragment;
+import android.database.Cursor;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.helper.ItemTouchHelper;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import org.cnx.android.R;
+import org.cnx.android.adapters.FavsRecyclerViewAdapter;
+import org.cnx.android.beans.Content;
+import org.cnx.android.providers.Favs;
+import org.cnx.android.providers.utils.DBUtils;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+import co.paulburke.android.itemtouchhelperdemo.helper.OnStartDragListener;
+import co.paulburke.android.itemtouchhelperdemo.helper.SimpleItemTouchHelperCallback;
+
+/**
+ * Fragment for displaying Favorites
+ * @author Ed Woodward
+ */
+public class FavsFragment extends Fragment implements OnStartDragListener
+{
+    /** Adaptor for card display */
+    FavsRecyclerViewAdapter adapter;
+    RecyclerView recyclerView;
+    /** list of bookmarks as Content objects */
+    ArrayList<Content> content;
+
+    /**handler */
+    final private Handler handler = new Handler();
+
+    /** Inner class for completing load work */
+    private Runnable finishedLoadingListTask = new Runnable()
+    {
+        public void run()
+        {
+            finishedLoadingList();
+        }
+    };
+
+    private ItemTouchHelper itemTouchHelper;
+
+    Activity activity;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+    {
+        activity = getActivity();
+        View v = inflater.inflate(R.layout.card_view, container, false);
+
+
+        return v;
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState)
+    {
+        super.onActivityCreated(savedInstanceState);
+        recyclerView = (RecyclerView)getView().findViewById(R.id.recycler_view);
+
+        recyclerView.setLayoutManager(new LinearLayoutManager(activity));
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+
+
+        //get already retrieved feed and reuse if it is there
+        content = (ArrayList<Content>)activity.getLastNonConfigurationInstance();
+        if(content == null)
+        {
+            //no previous data, so database must be read
+            readDB();
+        }
+        else
+        {
+            //reuse existing feed data
+            adapter = new FavsRecyclerViewAdapter(content, R.layout.card_row, activity);
+            recyclerView.setAdapter(adapter);
+            ItemTouchHelper.Callback callback = new SimpleItemTouchHelperCallback(adapter);
+            itemTouchHelper = new ItemTouchHelper(callback);
+            itemTouchHelper.attachToRecyclerView(recyclerView);
+            //setProgressBarIndeterminateVisibility(false);
+
+        }
+    }
+
+    /**
+     * For OnStartDragListener
+     * @param viewHolder The holder of the view to drag.
+     */
+    @Override
+    public void onStartDrag(RecyclerView.ViewHolder viewHolder)
+    {
+        itemTouchHelper.startDrag(viewHolder);
+    }
+
+    @Override
+    public void onResume()
+    {
+        super.onResume();
+        //if database state has changed, reload the display
+        if(content != null)
+        {
+            int dbCount = getDBCount();
+
+            if(dbCount >  content.size())
+            {
+                readDB();
+            }
+        }
+    }
+
+    /** Actions after list is loaded in View*/
+    protected void finishedLoadingList()
+    {
+        recyclerView.setAdapter(adapter);
+        ItemTouchHelper.Callback callback = new SimpleItemTouchHelperCallback(adapter);
+        itemTouchHelper = new ItemTouchHelper(callback);
+        itemTouchHelper.attachToRecyclerView(recyclerView);
+        //setProgressBarIndeterminateVisibility(false);
+    }
+
+    /** reads feed in a separate thread.  Starts progress dialog*/
+    private void readDB()
+    {
+        Thread loadFavsThread = new Thread()
+        {
+            public void run()
+            {
+
+                content = DBUtils.readCursorIntoList(activity.getContentResolver().query(Favs.CONTENT_URI, null, null, null, null));
+
+                Collections.sort(content);
+
+                fillData(content);
+                handler.post(finishedLoadingListTask);
+            }
+        };
+        loadFavsThread.start();
+
+    }
+    /**
+     * Loads feed data into adapter on initial reading of feed
+     * @param contentList ArrayList<Content>
+     */
+    private void fillData(ArrayList<Content> contentList)
+    {
+        //Log.d("LensViewer", "fillData() called");
+        adapter = new FavsRecyclerViewAdapter(contentList, R.layout.card_row,activity);
+    }
+
+    /**
+     * Queries the database to get the number of favorites stored
+     * @return int - the number of favorites items in the database
+     */
+    private int getDBCount()
+    {
+        Cursor c = activity.getContentResolver().query(Favs.CONTENT_URI, null, null, null, null);
+        int count = c.getCount();
+        c.close();
+        return count;
+
+    }
+
+}
